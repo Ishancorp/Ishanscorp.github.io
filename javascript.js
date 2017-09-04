d6f927e720ee240239dd7123c0e23548a0c8bbe3
Ishancorp <sharma.ishan450@gmail.com>
2017-09-04 18:18:28 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -7,12 +7,6 @@ var hour=d.getHours();
 var minute=d.getMinutes();
 var second=d.getSeconds();
 
-var date=document.getElementById("date");
-date.innerHTML="Date: "+months[month_num]+" "+day+", "+year;
-
-var time=document.getElementById("time");
-time.innerHTML="Time: "+hour+":"+minute+":"+second;
-
 var title=document.getElementById("uber_title");
 title.innerHTML="Ishancorp";
 
