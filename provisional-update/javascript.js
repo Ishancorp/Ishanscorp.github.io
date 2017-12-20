7bb79d94a3e9236dd7a688dbc3cb7cf9c7ebaae0
Ishancorp <sharma.ishan450@gmail.com>
2017-12-20 14:54:14 -0500

    Create javascript.js

---
+++ provisional-update/javascript.js
@@ -0,0 +1,24 @@
+var title=document.getElementById("uber_title");
+title.innerHTML="Ishan Sharma";
+
+var home=document.getElementById("nav1");
+home.innerHTML="Home";
+home.href="index.html"
+
+var naval=document.getElementById("nav2");
+naval.innerHTML="World War Sea";
+naval.href="naval_warfare.html"
+
+var about=document.getElementById("nav3");
+about.innerHTML="About";
+about.href="about.html"
+
+document.getElementById("menu").innerHTML='
+    <li class="topline"><a href="#">some text</a></li>
+    <li><a href="#">some text </a></li>
+    <li><a href="#">some text</a></li>
+    <li><a href="#">some text</a></li>
+    <li><a href="#"some text</a></li>
+    <li><a href="#">some text</a></li>
+    <li><a href="#">some text</a></li>
+';
