5474e91df6d6392fcde2ff5b251cb55e8068f91d
Ishan Sharma <sharma.ishan450@gmail.com>
2022-05-17 00:55:04 -0400

    Added config files and stuff

--- api_frontend/script.js
+++ api_frontend/script.js
@@ -17,12 +17,13 @@ prompt_maker.onsubmit = async (e) => {
             frequency_penalty: 0.0,
             presence_penalty: 0.0
         };
+        const API_KEY = config.API_KEY;
 
         let response = await fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                Authorization: `Bearer ${{ env.API_KEY }}`,
+                Authorization: `Bearer ${API_KEY}`,
             },
             body: JSON.stringify(data),
         });
